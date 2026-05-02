0a1c4ec100027de6afbf98a81009bbdab7febb58
Varvara Karyakina <var13ka@gmail.com>
2026-05-02 11:03:09 +0300

    add: focal loss

---
+++ emotion_detection/src/utils/focal_loss.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=2.0, pos_weight=None):
+        super().__init__()
+        self.gamma = gamma
+        self.pos_weight = pos_weight
+
+    def forward(self, logits, targets):
+        bce = F.binary_cross_entropy_with_logits(
+            logits, targets, pos_weight=self.pos_weight, reduction='none'
+        )
+        pt = torch.exp(-bce)
+        focal_loss = ((1 - pt) ** self.gamma * bce).mean()
+        return focal_loss
