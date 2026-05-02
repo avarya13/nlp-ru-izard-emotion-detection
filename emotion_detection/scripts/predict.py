47660a46deb10d8101b00a8a8b3ec3f995df8cb1
Varvara Karyakina <var13ka@gmail.com>
2026-05-02 16:36:29 +0300

    fix: config usage

--- emotion_detection/scripts/predict.py
+++ emotion_detection/scripts/predict.py
@@ -1,11 +1,21 @@
 import argparse
 import torch
+import os
+import hydra
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from omegaconf import OmegaConf
 
-cfg_labels = OmegaConf.load("../../configs/data/labels.yaml")
+
+with hydra.initialize(version_base="1.3", config_path="../conf"):
+    cfg = hydra.compose(config_name="config")
+
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+labels_path = os.path.join(script_dir, "../conf/data/labels.yaml")
+cfg_labels = OmegaConf.load(labels_path)
 LABELS = cfg_labels.labels
 LABELS_RU = cfg_labels.labels_ru
+
 
 def load_model(model_dir):
     tokenizer = AutoTokenizer.from_pretrained(model_dir)
@@ -13,25 +23,30 @@ def load_model(model_dir):
     model.eval()
     return tokenizer, model
 
+
 def predict_emotion(text, tokenizer, model, labels=LABELS):
     inputs = tokenizer(text, truncation=True, return_tensors="pt")
     with torch.no_grad():
         outputs = model(**inputs)
         probs = torch.sigmoid(outputs.logits).squeeze(0)
-    # multilabel: return probabilities for each class
     return {labels[i]: probs[i].item() for i in range(len(labels))}
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_path", default=f"{cfg.paths.save_dir}/{cfg.model.model_name.replace('/', '_')}")
+    default_path = f"{cfg.paths.save_dir}/{cfg.model.model_name.replace('/', '_')}"
+    parser.add_argument(
+        "--model_path", default=default_path, help="Path to saved model directory"
+    )
     parser.add_argument("--text", type=str, required=True)
     args = parser.parse_args()
 
     tokenizer, model = load_model(args.model_path)
     emotions = predict_emotion(args.text, tokenizer, model)
-    # sorting by decreasing
+
     for label, prob in sorted(emotions.items(), key=lambda x: -x[1]):
         print(f"{label.title().ljust(12)}: {prob:.4f}")
+
 
 if __name__ == "__main__":
     main()
