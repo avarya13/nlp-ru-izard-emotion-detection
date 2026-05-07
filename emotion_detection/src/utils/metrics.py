8b21a896588a9cf98013d2b142ffc474ec8bbb17
Varvara Karyakina <v.karyakina@smartengines.com>
2026-05-07 19:50:36 +0300

    upd: add custom f1

--- emotion_detection/src/utils/metrics.py
+++ emotion_detection/src/utils/metrics.py
@@ -1,12 +1,16 @@
-from sklearn.metrics import roc_auc_score, f1_score
-
-
-def compute_auc(y_true, y_pred):
-    """y_true, y_pred: numpy arrays of shape (n_samples, n_labels)"""
-    return roc_auc_score(y_true, y_pred, average=None)
-
+from sklearn.metrics import f1_score
+import pandas as pd
 
 def compute_f1_macro(y_true, y_pred):
-    # threshold 0.5
-    y_pred_bin = (y_pred > 0.5).astype(int)
-    return f1_score(y_true, y_pred_bin, average="macro")
+    df = pd.DataFrame([
+        {'macro': f1_score(y_true[:, i], y_pred[:, i] > 0.5, average='macro')}
+        for i in range(y_true.shape[1])
+    ])
+    return df['macro'].mean()
+
+def compute_f1_micro(y_true, y_pred):
+    df = pd.DataFrame([
+        {'micro': f1_score(y_true[:, i], y_pred[:, i] > 0.5, average='micro')}
+        for i in range(y_true.shape[1])
+    ])
+    return df['micro'].mean()
