cb9e8c8cb2adc6324ade8c84f987aacbe21d6f43
Varvara Karyakina <v.karyakina@smartengines.com>
2026-05-20 16:50:19 +0300

    upd: classifier and inference scripts

--- emotion_detection/scripts/infer.py
+++ emotion_detection/scripts/infer.py
@@ -6,8 +6,7 @@ from omegaconf import DictConfig
 from src.data.emotion_datamodule import EmotionDataModule
 from src.utils.dvc_pull import dvc_pull
 from src.utils.metrics import compute_f1_macro, compute_f1_micro
-from torchmetrics import AUROC, F1Score, Precision, Recall
-from torchmetrics.classification import MultilabelRankingLoss
+from torchmetrics import F1Score, Precision, Recall
 from transformers import AutoModelForSequenceClassification
 
 
@@ -18,7 +17,6 @@ def main(cfg: DictConfig):
         data_dir=cfg.data.data_dir,
         model_name=cfg.model.model_name,
         batch_size=cfg.data.batch_size,
-        # max_length=cfg.data.max_length,
         num_workers=cfg.data.num_workers,
     )
     dm.setup("test")
@@ -47,7 +45,6 @@ def main(cfg: DictConfig):
             model_path = base_path
             print(f"Base path does not exist: {base_path}")
 
-    # model_path = f"{cfg.paths.save_dir}/{cfg.model.model_name.replace('/', '_')}"
     model = AutoModelForSequenceClassification.from_pretrained(model_path)
     model.eval()
 
@@ -67,38 +64,28 @@ def main(cfg: DictConfig):
     y_true = torch.cat(all_labels).int()
 
     num_labels = cfg.model.num_labels
-    auroc = AUROC(task="multilabel", num_labels=num_labels)
     f1_macro = F1Score(task="multilabel", num_labels=num_labels, average="macro")
     f1_micro = F1Score(task="multilabel", num_labels=num_labels, average="micro")
     precision_macro = Precision(
         task="multilabel", num_labels=num_labels, average="macro"
     )
     recall_macro = Recall(task="multilabel", num_labels=num_labels, average="macro")
-    ranking_loss = MultilabelRankingLoss(num_labels=num_labels)
 
     print(f"probs: {y_probs}")
 
-    auc_score = auroc(y_probs, y_true)
     f1_macro_score = f1_macro(y_probs, y_true)
     f1_micro_score = f1_micro(y_probs, y_true)
     precision_score = precision_macro(y_probs, y_true)
     recall_score = recall_macro(y_probs, y_true)
-    ranking_loss.update(y_probs, y_true)
-    lrl = ranking_loss.compute().item()
 
-    print(f"Mean ROC-AUC: {auc_score:.4f}")
     print(f"Macro F1-score: {f1_macro_score:.4f}")
     print(f"Micro F1-score: {f1_micro_score:.4f}")
     print(f"Macro Precision: {precision_score:.4f}")
     print(f"Macro Recall: {recall_score:.4f}")
-    print(f"Label Ranking Loss: {lrl:.4f}")
 
-    # y_pred_bin = (y_probs > 0.5).int().cpu().numpy()
     y_true_np = y_true.cpu().numpy()
     y_probs_np = y_probs.cpu().numpy()
 
-    # labels_np = batch["labels"].int().cpu().detach().numpy()
-    # probs_np = probs.cpu().detach().numpy()
     test_f1_macro = compute_f1_macro(y_true_np, y_probs_np)
     test_f1_micro = compute_f1_micro(y_true_np, y_probs_np)
 
--- emotion_detection/src/models/multilabel_classifier.py
+++ emotion_detection/src/models/multilabel_classifier.py
@@ -6,8 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 from src.utils.focal_loss import FocalLoss
 from src.utils.metrics import compute_f1_macro, compute_f1_micro
-from torchmetrics import AUROC, F1Score, Precision, Recall
-from torchmetrics.classification import MultilabelRankingLoss
+from torchmetrics import F1Score, Precision, Recall
 from transformers import (
     AutoModelForSequenceClassification,
     get_linear_schedule_with_warmup,
@@ -31,6 +30,7 @@ class MultiLabelClassifier(L.LightningModule):
     ):
         super().__init__()
         self.save_hyperparameters()
+
         self.model = AutoModelForSequenceClassification.from_pretrained(
             model_name, num_labels=num_labels, problem_type="multi_label_classification"
         )
@@ -42,11 +42,6 @@ class MultiLabelClassifier(L.LightningModule):
             self.loss_fn = FocalLoss(gamma=focal_gamma, pos_weight=pos_weight)
         else:
             raise ValueError(f"Unknown loss_type: {loss_type}")
-
-        # metrics
-        self.train_auroc = AUROC(task="multilabel", num_labels=num_labels)
-        self.val_auroc = AUROC(task="multilabel", num_labels=num_labels)
-        self.test_auroc = AUROC(task="multilabel", num_labels=num_labels)
 
         self.train_f1_macro = F1Score(
             task="multilabel", num_labels=num_labels, average="macro"
@@ -60,7 +55,6 @@ class MultiLabelClassifier(L.LightningModule):
         self.train_recall_macro = Recall(
             task="multilabel", num_labels=num_labels, average="macro"
         )
-        self.train_ranking_loss = MultilabelRankingLoss(num_labels=num_labels)
 
         self.val_f1_macro = F1Score(
             task="multilabel", num_labels=num_labels, average="macro"
@@ -74,20 +68,6 @@ class MultiLabelClassifier(L.LightningModule):
         self.val_recall_macro = Recall(
             task="multilabel", num_labels=num_labels, average="macro"
         )
-        self.val_ranking_loss = MultilabelRankingLoss(num_labels=num_labels)
-
-        # self.test_f1_macro = F1Score(
-        #     task="multilabel", num_labels=num_labels, average="macro"
-        # )
-        # self.test_f1_micro = F1Score(
-        #     task="multilabel", num_labels=num_labels, average="micro"
-        # )
-        # self.test_precision_macro = Precision(
-        #     task="multilabel", num_labels=num_labels, average="macro"
-        # )
-        # self.test_recall_macro = Recall(
-        #     task="multilabel", num_labels=num_labels, average="macro"
-        # )
 
     def forward(self, input_ids, attention_mask):
         return self.model(input_ids=input_ids, attention_mask=attention_mask)
@@ -105,66 +85,23 @@ class MultiLabelClassifier(L.LightningModule):
         logits = outputs.logits
         probs = torch.sigmoid(logits)
 
-        self.train_auroc(probs, batch["labels"].int())
         self.train_f1_macro(probs, batch["labels"].int())
         self.train_f1_micro(probs, batch["labels"].int())
         self.train_precision_macro(probs, batch["labels"].int())
         self.train_recall_macro(probs, batch["labels"].int())
-        self.train_ranking_loss(probs, batch["labels"].int())
 
         labels_np = batch["labels"].int().cpu().detach().numpy()
         probs_np = probs.cpu().detach().numpy()
         custom_train_f1_macro = compute_f1_macro(labels_np, probs_np)
         custom_train_f1_micro = compute_f1_micro(labels_np, probs_np)
 
-        self.log("train_auroc", self.train_auroc, prog_bar=True)
         self.log("train_f1_macro", self.train_f1_macro, prog_bar=True)
         self.log("train_f1_micro", self.train_f1_micro, prog_bar=True)
         self.log("custom_train_f1_macro", custom_train_f1_macro, prog_bar=True)
         self.log("custom_train_f1_micro", custom_train_f1_micro, prog_bar=True)
         self.log("train_precision_macro", self.train_precision_macro, prog_bar=True)
         self.log("train_recall_macro", self.train_recall_macro, prog_bar=True)
-        self.log("train_ranking_loss", self.train_ranking_loss, prog_bar=True)
         return loss
-
-    # def on_train_epoch_end(self):
-    #     self.log(
-    #           "train_auroc",
-    #           self.train_auroc.compute(),
-    #           prog_bar=True
-    #     )
-    #     self.log(
-    #           "train_f1_macro",
-    #           self.train_f1_macro.compute(),
-    #           prog_bar=True
-    #     )
-    #     self.log(
-    #           "train_f1_micro",
-    #           self.train_f1_micro.compute(),
-    #           prog_bar=True
-    #     )
-    #     self.log(
-    #           "train_precision_macro",
-    #           self.train_precision_macro.compute(),
-    #           prog_bar=True
-    #     )
-    #     self.log(
-    #           "train_recall_macro",
-    #           self.train_recall_macro.compute(),
-    #           prog_bar=True
-    #     )
-    #     self.log(
-    #           "train_ranking_loss",
-    #           self.train_ranking_loss.compute(),
-    #           prog_bar=True
-    #     )
-
-    #     self.train_auroc.reset()
-    #     self.train_f1_macro.reset()
-    #     self.train_f1_micro.reset()
-    #     self.train_precision_macro.reset()
-    #     self.train_recall_macro.reset()
-    #     self.train_ranking_loss.reset()
 
     def validation_step(self, batch, batch_idx):
         outputs = self.model(
@@ -177,12 +114,10 @@ class MultiLabelClassifier(L.LightningModule):
         # loss = self.loss_fn(logits, batch["labels"])
         probs = torch.sigmoid(logits)
 
-        self.val_auroc(probs, batch["labels"].int())
         self.val_f1_macro(probs, batch["labels"].int())
         self.val_f1_micro(probs, batch["labels"].int())
         self.val_precision_macro(probs, batch["labels"].int())
         self.val_recall_macro(probs, batch["labels"].int())
-        self.val_ranking_loss(probs, batch["labels"].int())
 
         labels_np = batch["labels"].int().cpu().detach().numpy()
         probs_np = probs.cpu().detach().numpy()
@@ -190,59 +125,13 @@ class MultiLabelClassifier(L.LightningModule):
         custom_val_f1_micro = compute_f1_micro(labels_np, probs_np)
 
         self.log("val_loss", loss, prog_bar=True)
-        self.log("val_auroc", self.val_auroc, prog_bar=True)
         self.log("val_f1_macro", self.val_f1_macro, prog_bar=True)
         self.log("val_f1_micro", self.val_f1_micro, prog_bar=True)
         self.log("val_precision_macro", self.val_precision_macro, prog_bar=True)
         self.log("val_recall_macro", self.val_recall_macro, prog_bar=True)
         self.log("custom_val_f1_macro", custom_val_f1_macro, prog_bar=True)
         self.log("custom_val_f1_micro", custom_val_f1_micro, prog_bar=True)
-        self.log("val_ranking_loss", self.val_ranking_loss, prog_bar=True)
         return loss
-
-    # def on_validation_epoch_end(self):
-    #     self.log("val_auroc", self.val_auroc.compute(), prog_bar=True)
-    #     self.log("val_f1_macro", self.val_f1_macro.compute(), prog_bar=True)
-    #     self.log("val_f1_micro", self.val_f1_micro.compute(), prog_bar=True)
-    #     self.log(
-    #           "val_precision_macro",
-    #           self.val_precision_macro.compute(),
-    #           prog_bar=True
-    #     )
-    #     self.log("val_recall_macro", self.val_recall_macro.compute(), prog_bar=True)
-    #     self.log("val_ranking_loss", self.val_ranking_loss.compute(), prog_bar=True)
-
-    #     self.val_auroc.reset()
-    #     self.val_f1_macro.reset()
-    #     self.val_f1_micro.reset()
-    #     self.val_precision_macro.reset()
-    #     self.val_recall_macro.reset()
-    #     self.val_ranking_loss.reset()
-
-    # def test_step(self, batch, batch_idx):
-    #     outputs = self.model(
-    #         input_ids=batch["input_ids"],
-    #         attention_mask=batch["attention_mask"],
-    #         labels=batch["labels"],
-    #     )
-    #     logits = outputs.logits
-    #     probs = torch.sigmoid(logits)
-
-    #     self.test_auroc(probs, batch["labels"].int())
-    #     self.test_f1_macro(probs, batch["labels"].int())
-    #     self.test_f1_micro(probs, batch["labels"].int())
-    #     self.test_precision_macro(probs, batch["labels"].int())
-    #     self.test_recall_macro(probs, batch["labels"].int())
-
-    #     self.log("test_auroc", self.test_auroc, prog_bar=True)
-    #     self.log("test_f1_macro", self.test_f1_macro, prog_bar=True)
-    #     self.log("test_f1_micro", self.test_f1_micro, prog_bar=True)
-    #     self.log(
-    #           "test_precision_macro",
-    #           self.test_precision_macro,
-    #           prog_bar=True
-    #     )
-    #     self.log("test_recall_macro", self.test_recall_macro, prog_bar=True)
 
     def configure_optimizers(self):
         optimizer = optim.AdamW(
