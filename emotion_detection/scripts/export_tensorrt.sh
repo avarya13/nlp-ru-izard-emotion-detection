351c65e2cb620138ce45ee29e135ae7f86786885
Varvara Karyakina <v.karyakina@smartengines.com>
2026-05-21 17:21:55 +0300

    enh: script for tensorrt export

--- emotion_detection/scripts/export_tensorrt.sh
+++ emotion_detection/scripts/export_tensorrt.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-
 ONNX_PATH=$1
 ENGINE_PATH=$2
 
-trtexec \
-  --onnx=$ONNX_PATH \
-  --saveEngine=$ENGINE_PATH
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+mkdir -p $(dirname ${ROOT_DIR}/${ENGINE_PATH})
+
+docker run --rm --gpus all \
+  -v ${ROOT_DIR}:/workspace \
+  nvcr.io/nvidia/tensorrt:24.12-py3 \
+  trtexec \
+    --onnx=/workspace/${ONNX_PATH} \
+    --saveEngine=/workspace/${ENGINE_PATH} \
+    --verbose
