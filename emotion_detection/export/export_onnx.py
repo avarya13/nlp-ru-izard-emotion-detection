a0eb6f2903e6bb4f6049603754333499b32ab2e6
Varvara Karyakina <v.karyakina@smartengines.com>
2026-05-23 16:33:36 +0300

    enh: script for onnx export

--- emotion_detection/export/export_onnx.py
+++ emotion_detection/export/export_onnx.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from pathlib import Path
 
 import torch
@@ -6,7 +5,6 @@ from omegaconf import DictConfig
 from transformers import AutoModelForSequenceClassification
 
 
-# @hydra.main(version_base="1.3", config_path="../../configs", config_name="config")
 def export_onnx(cfg: DictConfig):
     base_model_dir = Path(cfg.paths.save_dir) / cfg.model.model_name.replace("/", "-")
 
@@ -15,7 +13,7 @@ def export_onnx(cfg: DictConfig):
     if timestamp:
         model_dir = base_model_dir / timestamp
         print(f"Using specified model directory: {model_dir}")
-
+        ts = timestamp
     else:
         if base_model_dir.exists():
             subdirs = [
@@ -23,29 +21,21 @@ def export_onnx(cfg: DictConfig):
                 for d in base_model_dir.iterdir()
                 if d.is_dir() and len(d.name) == 13 and d.name[8] == "-"
             ]
-
             if not subdirs:
                 raise ValueError(
                     f"No timestamped model directories found in {base_model_dir}"
                 )
-
             latest = max(subdirs, key=lambda d: d.name)
             model_dir = latest
-
+            ts = latest.name
             print(f"Using latest model directory: {model_dir}")
-
         else:
             raise ValueError(f"Model directory does not exist: {base_model_dir}")
 
     model = AutoModelForSequenceClassification.from_pretrained(model_dir)
-
     model.eval()
 
-    output_dir = Path(cfg.paths.onnx_dir) / cfg.model.model_name.replace("/", "-")
-
-    export_timestamp = datetime.now().strftime("%Y%m%d-%H%M")
-
-    output_dir = output_dir / export_timestamp
+    output_dir = Path(cfg.paths.onnx_dir) / cfg.model.model_name.replace("/", "-") / ts
     output_dir.mkdir(parents=True, exist_ok=True)
 
     onnx_path = output_dir / "model.onnx"
@@ -83,7 +73,7 @@ def export_onnx(cfg: DictConfig):
                 0: "batch_size",
             },
         },
-        opset_version=14,
+        opset_version=17,
         do_constant_folding=False,
         export_params=True,
         keep_initializers_as_inputs=False,
